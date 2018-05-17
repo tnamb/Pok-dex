cf45b7721f8430c430b896919b23395262e23a87
tnamb <37467040+tnamb@users.noreply.github.com>
2018-05-17 10:32:35 +0530

    SomeNewCodesbleh
    bleh

--- Pokemon/connection.php
+++ Pokemon/connection.php
@@ -1,7 +1,7 @@
 <?php
 $db_host='localhost';
-$db_user='presi';
-$db_password='1234';
+$db_user='root';
+$db_password='tnamb007';
 $db_name='pokedex';
 
 $dbc=mysqli_connect($db_host,$db_user,$db_password,$db_name)
